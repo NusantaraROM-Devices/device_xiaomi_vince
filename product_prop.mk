3dbef4666068a7d2f979e415df194e2ebec24d2c
Bladeyy <getsuga369@gmail.com>
2019-06-24 18:51:50 +0530

    vince: Enable USB debugging at boot
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: Kry9toN <dhimasbagusprayoga@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -12,3 +12,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Maintainer
 PRODUCT_PROPERTY_OVERRIDES += \
     org.komodo.maintainer=Kry9toN
+
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
