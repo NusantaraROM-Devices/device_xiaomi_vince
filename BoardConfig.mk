935928807b76669ec101a7caed89a61bc8d39d03
ZeeLog <Danjel.Zammit@gmail.com>
2020-06-06 10:44:14 +0200

    vince: Move/Add rild properties to system.prop
    * from CAF LA.UM.8.6.r1-04400-89xx.0
    
    Change-Id: I3485c0f8177d4be96bd648fe864d307615b08703

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,6 +40,8 @@ BUILD_BROKEN_DUP_RULES := true
 TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true
 
+TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.usbconfigfs=true loop.max_part=16
