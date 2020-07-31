a72ade7562d29276604f2eaf028ad556087ac052
Kry9toN <dhimasbagusprayoga@gmail.com>
2020-07-31 18:40:47 +0700

    Revert "vince: build AOSP light HIDL"
    This reverts commit c8301afc6301405930a4b5f5df39ef886997782b.

--- device.mk
+++ device.mk
@@ -254,9 +254,12 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8953
+    lights.msm8953 \
+    android.hardware.light@2.0-service.xiaomi_vince
+
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_PACKAGES += \
