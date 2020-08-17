8e19e3c668c419c2c7ecf29a6ea6481594ac4139
Yo Chiang <yochiang@google.com>
2020-08-17 11:44:54 +0800

    vince: Use on_vendor VNDK APEX
    Move VNDK APEX to /vendor partition so we don't need to update
    vendor.img prebuilt everytime there is a change in the VNDK
    libraries.
    
    Change-Id: I832191e8876306883a04c8539cf8dcda16fe940f
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -462,6 +462,7 @@ PRODUCT_COPY_FILES += \
 $(foreach target, $(shell cat $(LOCAL_PATH)/vndk.txt), $(eval PRODUCT_PACKAGES += $(target).vendor))
 
 PRODUCT_PACKAGES += \
+    com.android.vndk.current.on_vendor \
     libstdc++.vendor
 
 PRODUCT_COPY_FILES += \
