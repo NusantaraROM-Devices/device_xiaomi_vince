3db068d2e519de97e4e17d60313a0bf9a27606d4
Jabiyeff <cebiyevanar@gmail.com>
2021-03-26 14:56:38 +0400

    vince: Remove power-stats HAL support for now
    * It doesn't work stable on Android 12
    
    11-03 00:38:51.734   581   581 E android.hardware.power.stats@1.0-service-mock: Failed to getEnergyData
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- device.mk
+++ device.mk
@@ -358,8 +358,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service-qti
 
 # Powerhint (system/vendor)
 PRODUCT_COPY_FILES += \
