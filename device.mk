49388f4b4132423cafc73826d1b24d7db47b243b
Erfan Abdi <erfangplus@gmail.com>
2021-09-10 20:54:14 +0200

    vince: prepare to move to in-tree HALs
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>
    Change-Id: I6469959966bdda060a5c1ecc104ca0318334c6a1

--- device.mk
+++ device.mk
@@ -42,6 +42,9 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Sna
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+QCOM_SOONG_NAMESPACE := \
+    $(LOCAL_PATH)/qcom-caf
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
