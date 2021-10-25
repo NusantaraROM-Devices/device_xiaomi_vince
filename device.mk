c16ce5cd825f3da6f8edfdd4b2f47e9ca119c30b
Kingsman44 <Kingsman44@users.noreply.github.com>
2021-10-25 12:06:46 +0530

    vince: Update Perf Stack from thyme qssi-user 12 SKQ1.210908.001 21.10.12 release-keys
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -352,10 +352,6 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.0.vendor \
-    vendor.qti.hardware.perf@2.1.vendor \
-    vendor.qti.hardware.perf@2.2 \
-    vendor.qti.hardware.perf@2.2.vendor \
     android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
