81bcc36192a341fcf4e68a9c2acece197c96d8e8
grindklast <fhisatyn@gmail.com>
2022-03-23 12:11:16 +0700

    vince: refactor scripts for Nusantara

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -28,9 +28,9 @@ INITIAL_COPYRIGHT_YEAR=2017
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ROM_ROOT="${MY_DIR}/../../.."
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${ROM_ROOT}/vendor/havoc/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/vendor/nusantara/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -38,7 +38,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ROM_ROOT}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
 # Copyright headers and guards
 write_headers
